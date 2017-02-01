e9aa01dacd5a5f7ceba39271a87e0418b68975fe
Adrian Lobdill <slolobdill44@gmail.com>
2017-01-31 23:11:57 -0800

    Added basic tweet scrolling functionality

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 
 class Sidebar extends React.Component {
-  // constructor(props) {
-  //
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tweets: []
+    };
+
+    this.setState = this.setState.bind(this);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.tweets) {
+      this.setState({tweets: newProps.tweets});
+      console.log(newProps.tweets);
+    }
+  }
 
   render () {
+
+    const tweetList = this.state.tweets.map((tweet, idx) => {
+
+      return (
+        <li key={idx} className='tweet-list-item'>
+          <p>{tweet.text}</p>
+          <p> - {tweet.user_name}</p>
+        </li>
+      );
+    });
 
     return (
       <aside className='sidebar'>
         <ul>
-          <li className='tweet-list-item'>
-
-          </li>
-          <li className='tweet-list-item'>
-
-          </li>
+          {tweetList}
         </ul>
       </aside>
     );
--- frontend/components/sidebar/sidebar_container.jsx
+++ frontend/components/sidebar/sidebar_container.jsx
@@ -2,8 +2,10 @@ import { connect } from 'react-redux';
 import Sidebar from './sidebar';
 
 const mapStateToProps = state => ({
-
+  tweets: state.tweets.tweets,
+  tweet: state.tweets.tweet
 });
+
 const mapDispatchToProps = dispatch => ({
 
 });
