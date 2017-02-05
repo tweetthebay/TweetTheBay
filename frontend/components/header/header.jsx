bff4f49f1a65ee0219b014230c15ef9fcef79755
Adrian Lobdill <slolobdill44@gmail.com>
2017-02-04 16:56:52 -0800

    Fitted whole app in view port, app color changes on toggle switch

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,30 +3,51 @@ import ReactDOM from 'react-dom';
 import SearchContainer from '../search/search_container';
 
 import AppBar from 'material-ui/AppBar';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
 import FontIcon from 'material-ui/FontIcon';
 import Toggle from 'material-ui/Toggle';
 import Help from 'material-ui/svg-icons/action/help';
+import Paper from 'material-ui/Paper';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggleText: "search"
+      toggleText: "search",
+      open: true
     };
 
+    this.handleRequestClose = this.handleRequestClose.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+  }
+
+  // handleTouchTap(e) {
+  //   e.preventDefault();
+  //
+  //   this.setState({
+  //     open: true,
+  //     anchorEl: event.currentTarget,
+  //   });
+  // }
+
+  handleRequestClose() {
+    this.setState({
+      open: false
+    });
   }
 
   handleToggle() {
     var search = document.querySelector(".search-container");
     if (this.state.toggleText === "search") {
       $(".header > div").css("background-color", "#424874");
+      $(".footer").css("background-color", "#424874");
       search.style.visibility = "hidden";
       this.setState({toggleText: "streaming"});
     } else {
       $(".header > div").css("background-color", "rgb(0, 132, 180)");
+      $(".footer").css("background-color", "#02344A");
       search.style.visibility = "visible";
       this.setState({toggleText: "search"});
 
@@ -49,10 +70,25 @@ class Header extends React.Component {
 
   render() {
 
+    const logoStyle = {
+      fontSize: "55px",
+      marginBottom: "9px",
+      ariaHidden: "true",
+      color: "white"
+    };
+
     const helpStyle = {
       color: "white",
       marginRight: "25px",
       marginTop: "14px"
+    };
+
+    const tooltipStyle = {
+      height: 100,
+      width: 100,
+      margin: 20,
+      textAlign: 'center',
+      display: 'inline-block',
     };
 
     return(
@@ -62,12 +98,7 @@ class Header extends React.Component {
             titleStyle={{marginTop: "6px", }}
             iconElementLeft={<FontIcon
                               className="fa fa-twitter-square"
-                              style={{
-                                fontSize: "55px",
-                                marginBottom: "9px",
-                                ariaHidden: "true",
-                                color: "white"
-                              }}/>}
+                              style={logoStyle}/>}
             iconElementRight={<Help
                               style={helpStyle}
                               className="help-button"
@@ -76,8 +107,9 @@ class Header extends React.Component {
                         <div className="search-container">
                           <SearchContainer />
                         </div>
-                        <h3 className="search-toggle-text">
-                          Search
+                        <h3 className="search-toggle-text"
+                          onTouchTap={this.handleTouchTap}>Search
+
                         </h3>
                         <Toggle
                           className="toggle"
@@ -85,11 +117,18 @@ class Header extends React.Component {
                           style={{
                             width: "0px",
                             marginTop: "4px",
-                          }} />
+                          }}
+                          thumbStyle={{
+                            backgroundColor: "#3F477F"
+                          }}
+                          trackStyle={{
+                            backgroundColor: "#9EA4D1"
+                          }}/>
                         <h3 className="stream-toggle-text">
                           Tweet Stream
                         </h3>
                       </div>}
+
             style={{
               backgroundColor: '#0084b4',
               paddingLeft: "70px"
