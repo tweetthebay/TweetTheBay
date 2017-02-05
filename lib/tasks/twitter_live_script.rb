e69ca732557f05df8ca69a0f6f63eef557e560d1
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-02-05 14:41:10 -0800

    Update script blacklist, stream map marker fix

--- lib/tasks/twitter_live_script.rb
+++ lib/tasks/twitter_live_script.rb
@@ -14,25 +14,25 @@ def farallon_islands_spam?(tweet_coordinates_hash)
   long.between?(-123.1, -122.9) && lat.between?(37.6, 37.8)
 end
 
-def bay_area_coordinates?(tweet_coordinates_hash)
+def not_bay_area_coordinates?(tweet_coordinates_hash)
   coordinates_array = tweet_coordinates_hash[:coordinates]
   long = coordinates_array[0]
   lat = coordinates_array[1]
   !long.between?(-123.632497, -121.4099121094) || !lat.between?(36.9476967925, 38.5288302896)
 end
 
-screen_name_whitelist = ["Job", "job", "Jobs", "job", "Careers", "careers", "test5geo1798", "tmj_"]
-job_posting_whitelist = ["See our latest", "We're #hiring!",
+screen_name_blacklist = ["Job", "job", "Jobs", "job", "Careers", "careers", "test5geo1798", "tmj_", "WorkAt"]
+job_posting_blacklist = ["See our latest", "We're #hiring!",
   "Read about our latest #job opening", "Want to work in",
   "Can you recommend anyone for this", "Want to work at",
   "If you're looking for work in"]
 
 newclient.filter(locations: "-123.632497,36.9476967925,-121.4099121094,38.5288302896") do |tweet|
   if tweet.attrs[:coordinates]
-    unless job_posting_whitelist.any? { |phrase| tweet.text.include?(phrase) } ||
-      screen_name_whitelist.any? { |term| tweet.user.screen_name.include?(term) } ||
+    unless job_posting_blacklist.any? { |phrase| tweet.text.include?(phrase) } ||
+      screen_name_blacklist.any? { |term| tweet.user.screen_name.include?(term) } ||
       farallon_islands_spam?(tweet.attrs[:coordinates]) ||
-      bay_area_coordinates?(tweet.attrs[:coordinates])
+      not_bay_area_coordinates?(tweet.attrs[:coordinates])
 
       Tweet.create!(
       text: tweet.text,
@@ -43,12 +43,6 @@ newclient.filter(locations: "-123.632497,36.9476967925,-121.4099121094,38.528830
       description: tweet.user.description,
       profile_picture: tweet.user.to_h[:profile_image_url_https],
       coordinates: tweet.to_h[:coordinates],
-      place_type: tweet.to_h[:place][:place_type],
-      place_name: tweet.to_h[:place][:name],
-      place_full_name: tweet.to_h[:place][:full_name],
-      place_country_code: tweet.to_h[:place][:country_code],
-      place_country: tweet.to_h[:place][:country],
-      place_bounding_box_coordinates: tweet.to_h[:place][:bounding_box][:coordinates].to_s,
       retweet_count: tweet.retweet_count,
       favorite_count: tweet.favorite_count,
       tweet_id: tweet.to_h[:id_str],
