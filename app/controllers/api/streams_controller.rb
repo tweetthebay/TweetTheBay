046684c233822d2efacd6fab16c01d53c6219e65
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-05-12 19:24:29 -0700

    Disable Redis

--- app/controllers/api/streams_controller.rb
+++ app/controllers/api/streams_controller.rb
@@ -2,11 +2,20 @@ class Api::StreamsController < ApplicationController
   def index
     time_mount_utc = params[:timeNowUTC].to_i
 
-    @streamTweets = []
+    @stream_tweets = []
 
-    Rails.cache.data.keys.sort.last(100).reverse.each do |key|
-      @streamTweets << Rails.cache.read(key) if Rails.cache.read(key).time_utc > time_mount_utc
+    if Tweet.where("time_utc > #{time_mount_utc}").empty?
+      @stream_tweets = Tweet.last(1)
+    else
+      @stream_tweets = Tweet
+        .where("time_utc > #{time_mount_utc}")
+        .last(100)
+        .reverse
     end
+
+    # Rails.cache.data.keys.sort.last(100).reverse.each do |key|
+    #   @streamTweets << Rails.cache.read(key) if Rails.cache.read(key).time_utc > time_mount_utc
+    # end
 
     render :index
   end
