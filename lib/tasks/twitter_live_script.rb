c21054e52b206f6db7cf3f6d06663fc66eedd651
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-05-12 22:02:45 -0700

    Add comments and clean code

--- lib/tasks/twitter_live_script.rb
+++ lib/tasks/twitter_live_script.rb
@@ -1,3 +1,5 @@
+# lib/tasks/twitter_live_script.rb
+
 require 'twitter'
 
 stream = Twitter::Streaming::Client.new do |config|
@@ -7,6 +9,7 @@ stream = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
+# Filter out suspiciously geolocated tweets
 def farallon_islands_spam?(tweet_coordinates_hash)
   coordinates_array = tweet_coordinates_hash[:coordinates]
   long = coordinates_array[0]
@@ -14,6 +17,7 @@ def farallon_islands_spam?(tweet_coordinates_hash)
   long.between?(-123.1, -122.9) && lat.between?(37.6, 37.8)
 end
 
+# Filter out incorrect geolocated tweets from Twitter API
 def not_bay_area_coordinates?(tweet_coordinates_hash)
   coordinates_array = tweet_coordinates_hash[:coordinates]
   long = coordinates_array[0]
@@ -21,13 +25,21 @@ def not_bay_area_coordinates?(tweet_coordinates_hash)
   !long.between?(-123.632497, -121.4099121094) || !lat.between?(36.9476967925, 38.5288302896)
 end
 
-screen_name_blacklist = ["Job", "job", "Jobs", "job", "Careers", "careers", "test5geo1798", "tmj_", "WorkAt"]
-job_posting_blacklist = ["See our latest", "We're #hiring!",
-  "Read about our latest #job opening", "Want to work in",
-  "Can you recommend anyone for this", "Want to work at",
-  "If you're looking for work in"]
+# Blacklists remove job posting accounts & tweets
+screen_name_blacklist = [
+  "Job", "job", "Jobs", "job", "Careers", "careers", "test5geo1798",
+  "tmj_", "WorkAt"
+]
 
+job_posting_blacklist = [
+  "See our latest", "We're #hiring!", "Read about our latest #job opening",
+  "Want to work in", "Can you recommend anyone for this",
+  "Want to work at", "If you're looking for work in"
+]
+
+# Coordinates set up, roughly, to the greater SF Bay Area
 stream.filter(locations: "-123.632497,36.9476967925,-121.4099121094,38.5288302896") do |tweet|
+  # Only consider tweets with proper geolocation data
   if tweet.attrs[:coordinates]
     unless job_posting_blacklist.any? { |phrase| tweet.text.include?(phrase) } ||
       screen_name_blacklist.any? { |term| tweet.user.screen_name.include?(term) } ||
@@ -50,10 +62,8 @@ stream.filter(locations: "-123.632497,36.9476967925,-121.4099121094,38.528830289
       time_utc: (Time.now.to_i * 1000)
       )
 
+      # Write tweet data to Redis via Rails cache
       Rails.cache.write(cache_tweet.id, cache_tweet)
-
-      puts tweet
-
     end
   end
 end
