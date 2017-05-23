f85c00949b4cf9eba49e64cf45c53f4250c2866e
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-05-23 12:12:21 -0700

    Add CurrentTrendReducer tests

---
+++ frontend/__tests__/reducer/current_trend_reducer-test.js
@@ -0,0 +1,81 @@
+/* globals jest */
+
+import CurrentTrendReducer from '../../reducers/current_trend_reducer';
+
+describe('Reducers', () => {
+  describe('CurrentTrendReducer', () => {
+    const _oldState = {
+      'trends': [
+        {
+          name: "#DubNation",
+          volume: 46800
+        },
+        {
+          name: "Giants",
+          volume: 33620
+        }
+      ]
+    };
+
+    it('exports an function', () => {
+      expect(typeof CurrentTrendReducer).toEqual('function');
+    });
+
+    it('should initialize with an empty array as the default state', () => {
+      expect(CurrentTrendReducer(undefined, {})).toEqual([]);
+    });
+
+    it('should return the previous state if an action is not matched', () => {
+      const newState = CurrentTrendReducer(_oldState, { type: 'notAType' });
+      expect(newState).toEqual(_oldState);
+    });
+
+    describe('handling the RECEIVE_CURRENT_TRENDS action', () => {
+      let currentTrends,
+          receiveCurrentTrendsAction;
+
+      beforeEach(() => {
+        currentTrends = {
+          'trends': [
+            {
+              name: "Burritos",
+              volume: 74330
+            },
+            {
+              name: "Tacos",
+              volume: 43620
+            }
+          ]
+        };
+
+        receiveCurrentTrendsAction = {
+          type: 'RECEIVE_CURRENT_TRENDS',
+          currentTrends
+        };
+      });
+
+      it('should replace the state with the receiveCurrentTrendsAction\'s currentTrends', () => {
+        const state = CurrentTrendReducer(undefined, receiveCurrentTrendsAction);
+        expect(state).toEqual(currentTrends);
+      });
+
+      it('should not modify the old state', () => {
+        const oldState = {
+          'trends': [
+            {
+              name: "#DubNation",
+              volume: 46800
+            },
+            {
+              name: "Giants",
+              volume: 33620
+            }
+          ]
+        };
+
+        CurrentTrendReducer(oldState, receiveCurrentTrendsAction);
+        expect(oldState).toEqual(_oldState);
+      });
+    });
+  });
+});
