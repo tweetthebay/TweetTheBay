a00335127acea6bfe370f39546e227c1a0cf0e98
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-05-22 22:03:39 -0700

    Add Current Tweet Actions tests

---
+++ frontend/__tests__/actions/current_tweet_actions-test.js
@@ -0,0 +1,49 @@
+/* globals jest */
+
+import {
+  SET_CURRENT_TWEET,
+  setCurrentTweet
+} from '../../actions/current_tweet_actions';
+
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+
+describe('current tweet actions', () => {
+  describe('current tweet constants', () => {
+    it('should contain a SET_CURRENT_TWEET constant', () => {
+      expect(SET_CURRENT_TWEET).toEqual('SET_CURRENT_TWEET');
+    });
+  });
+
+  describe('actions', () => {
+    let store;
+
+    beforeEach(() => {
+      store = mockStore({ currentTweet: {} });
+    });
+
+    it('should have a type of SET_CURRENT_TWEET', () => {
+      expect(setCurrentTweet().type).toEqual('SET_CURRENT_TWEET');
+    });
+
+    it('should pass on the tweet we pass in', () => {
+      const currentTweet = { id: 2 };
+      expect(setCurrentTweet(currentTweet).tweet).toEqual(currentTweet);
+    });
+
+    it ('should pass on the tweet we pass in (with mock store)', () => {
+      const currentTweet = { id: 2 };
+
+      const expectedActions = [{
+        type: "SET_CURRENT_TWEET",
+        tweet: currentTweet
+      }];
+
+      store.dispatch(setCurrentTweet(currentTweet));
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
