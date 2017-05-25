27158a9d2230a3e9e1c86316ac88eb75e78223f9
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-05-25 12:20:19 -0700

    Add StreamApiUtil tests

---
+++ frontend/__tests__/util/stream_api_util-test.js
@@ -0,0 +1,40 @@
+/* globals jest */
+
+import {
+  fetchStream,
+  fetchStreamSince
+} from '../../util/stream_api_util';
+
+describe('the stream api util', () => {
+  beforeEach(() => {
+    global.$ = require.requireMock('jquery');
+    global.$.ajax = jest.fn(options => "ajax promise");
+  });
+
+  afterEach(() => {
+    global.$.ajax.mockClear();
+  });
+
+  it('fetchStream makes request and returns an ajax promise', () => {
+    const returnValue = fetchStream();
+    expect($.ajax).toBeCalled();
+
+    // This line gets the first argument of the first call to $.ajax
+    const ajaxCallArg = $.ajax.mock.calls[0][0];
+    expect(ajaxCallArg.url).toEqual('api/streams');
+    expect(ajaxCallArg.type || ajaxCallArg.method).toMatch('GET');
+    expect(returnValue).toEqual("ajax promise");
+  });
+
+  it('fetchStreamSince makes request and returns an ajax promise', () => {
+    const timeNowUTC = Date.now()
+    const returnValue = fetchStreamSince(timeNowUTC);
+    expect($.ajax).toBeCalled();
+
+    const ajaxCallArg = $.ajax.mock.calls[0][0];
+    expect(ajaxCallArg.url).toEqual('api/streams');
+    expect(ajaxCallArg.type || ajaxCallArg.method).toMatch('GET');
+    expect(ajaxCallArg.data).toEqual({ timeNowUTC })
+    expect(returnValue).toEqual("ajax promise");
+  });
+});
