0241128aaa61249aada4b7f362b2b490f68bff59
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-06-07 22:55:32 -0700

    Prettify Store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,12 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState: Object = {}) => (
-  createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(thunk)
-  )
-);
+const configureStore = (preloadedState: Object = {}) =>
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 export default configureStore;
