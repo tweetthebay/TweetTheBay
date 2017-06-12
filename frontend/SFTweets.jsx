1df9f91c8f64d526cf55121445e53f57565aff00
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-06-11 22:35:58 -0700

    Update Entry file with new linting

--- frontend/SFTweets.jsx
+++ frontend/SFTweets.jsx
@@ -1,16 +1,19 @@
+// frontend/SFTweets.jsx
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
 import Modal from 'react-modal';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
+  const store = configureStore();
   Modal.setAppElement(document.body);
 
   injectTapEventPlugin();
 
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store}/>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
