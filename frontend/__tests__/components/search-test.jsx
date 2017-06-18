e3f2a2a558f3511740fac6fdac56c4149c06fc4f
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-06-18 12:33:32 -0700

    Increase Search Component test coverage

--- frontend/__tests__/components/search-test.jsx
+++ frontend/__tests__/components/search-test.jsx
@@ -1,39 +1,38 @@
 import React from 'react';
 import thunk from 'redux-thunk';
+import sinon from 'sinon';
 import configureMockStore from 'redux-mock-store';
 import { mount } from 'enzyme';
 import SearchContainer from '../../components/search/search_container';
 
 const window = document.defaultView;
-global.window = window
+global.window = window;
 global.$ = require('jquery');
 
 const state = {
   mapPosition: {
     lat: 37.9,
     lng: -122.5,
-    radius: 42.99316579561581
+    radius: 42.99316579561581,
   },
-  searchQuery: 'taco'
-}
+  searchQuery: 'taco',
+};
 
-const middlewares = [ thunk ];
+const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 const testStore = mockStore({
   mapPosition: state.mapPosition,
-  searchQuery: state.searchQuery
+  searchQuery: state.searchQuery,
 });
 
 describe('search container', () => {
-  let searchWrapper,
-      searchBody;
+  let searchWrapper;
+  let searchBody;
 
   describe('creating a new search', () => {
     beforeEach(() => {
-      searchWrapper = mount(
-        <SearchContainer store={testStore} />
-      ).find('Search');
+      searchWrapper = mount(<SearchContainer store={testStore} />).find('Search');
 
       searchBody = searchWrapper.find('input');
     });
@@ -49,8 +48,24 @@ describe('search container', () => {
     });
 
     test('updates the rating field when it changes', () => {
-      searchBody.simulate('change', { target: { value: 'potato' }});
+      searchBody.simulate('change', { target: { value: 'potato' } });
       expect(searchBody.props().value).toEqual('potato');
+    });
+
+    test('search component has handleSubmit function', () => {
+      searchBody.simulate('submit', { target: { value: 'potato' } });
+      expect(searchWrapper.props().setCurrentTweet(null)).toEqual({
+        tweet: null,
+        type: 'SET_CURRENT_TWEET',
+      });
+      expect(searchWrapper.props().setSearchQuery('potato')).toEqual({
+        query: 'potato',
+        type: 'RECEIVE_SEARCH_QUERY',
+      });
+      const searchPromise = searchWrapper
+        .props()
+        .searchTweets('potato', searchWrapper.props().location);
+      expect(typeof searchPromise.then).toEqual('function');
     });
   });
 });
